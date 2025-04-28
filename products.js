89d5bf65a66c1f7c8685de8ed6bd888e42745c91
jacopopesare <pesarejacopo1@gmail.com>
2025-04-28 13:06:15 +0200

    products.js

---
+++ products.js
@@ -0,0 +1,34 @@
+const products = [
+    {
+        id: 1,
+        name: "Funko Pop Spider-Man",
+        category: "Funko Pop",
+        price: 19.99,
+        image: "assets/product-images/funko1.png",
+        description: "Fantastico Funko Pop di Spider-Man con dettagli esclusivi."
+    },
+    {
+        id: 2,
+        name: "Funko Pop Batman",
+        category: "Funko Pop",
+        price: 18.99,
+        image: "assets/product-images/funko2.png",
+        description: "Iconico Funko Pop di Batman in edizione limitata."
+    },
+    {
+        id: 3,
+        name: "Action Figure Iron Man",
+        category: "Action Figure",
+        price: 39.99,
+        image: "assets/product-images/action1.png",
+        description: "Action figure altamente dettagliata di Iron Man."
+    },
+    {
+        id: 4,
+        name: "Action Figure Goku",
+        category: "Action Figure",
+        price: 34.99,
+        image: "assets/product-images/action2.png",
+        description: "Action figure di Goku in posa da combattimento."
+    }
+];
